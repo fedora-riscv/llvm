c9c45596f55e6cc2025315dfcd6fcdfbf7834d0d
Tom Stellard <tstellar@redhat.com>
2025-10-03 03:38:50 +0000

    Add a script for syncing the Fedora dist-git to CentOS Stream dist-git

---
+++ centos-sync.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -ex
+
+centos_branch=c10s
+centos_dir=llvm-centos
+fedora_dir=llvm-fedora
+fedora_ref=rawhide
+
+bundle=1
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    --no-bundle )
+      bundle=0
+      ;;
+    --centos-branch )
+      shift
+      centos_branch=$1
+      ;;
+    --fedora-ref )
+      shift
+      fedora_ref=$1
+      ;;
+    * )
+      echo "unknown option $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+git clone https://gitlab.com/redhat/centos-stream/rpms/llvm.git $centos_dir
+git -C $centos_dir checkout $centos_branch
+git clone https://src.fedoraproject.org/rpms/llvm.git $fedora_dir
+git -C $fedora_dir checkout $fedora_ref
+if [ $bundle -eq 1 ]; then
+  sed -i 's/^%bcond_with bundle_compat_lib$/%bcond_without bundle_compat_lib/g' $fedora_dir/llvm.spec
+fi
+
+files_to_copy=`git -C $fedora_dir/ ls-files | tr '\n' ' '`
+rsync --exclude=.packit.yaml --delete --cvs-exclude -av $fedora_dir/ $centos_dir/
+
+for f in $centos_dir/tests/*; do
+  sed -i 's~https://src.fedoraproject.org/tests/llvm.git~https://gitlab.com/redhat/centos-stream/tests/llvm.git~g' $f
+done
