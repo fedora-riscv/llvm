b36dc5eb11b0871a77a95c3f7cd7d559aafaec25
Tulio Magno Quites Machado Filho <tuliom@redhat.com>
2026-01-28 15:46:23 -0300

    Add support for compat repos to sync.sh
    Turn sync.sh into a generic synchronization script that is able
    to treat Fedora compat repositories too.
    
    This is important because compat repositories are more complex now and
    require many more files instead of just the spec file.
    
    Rename .centos-ignore file to .sync-ignore.

--- sync.sh
+++ sync.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+# Synchronize code from Rawhide to Fedora compat or CentOS.
+
 set -ex
 
-centos_branch=c10s
-centos_dir=llvm-centos
+CENTOS_REF=c10s
+CENTOS_DIR=llvm-centos
+
+COMPAT_REF=rawhide
+COMPAT_DIR=llvm-compat
+
 fedora_dir=llvm-fedora
 fedora_ref=rawhide
 
+
+
+function error() {
+  echo "Error: $1" >&2
+  exit 1
+}
+
+# Default parameter values.
 bundle=1
+initialize=0
+target="centos"
 
 script_dir=$(dirname $0)
 
@@ -16,9 +32,15 @@ while [ $# -gt 0 ]; do
     --no-bundle )
       bundle=0
       ;;
-    --centos-branch )
+    --initialize )
+      initialize=1
+      ;;
+    --compat )
+      target="compat"
+      ;;
+    --out-ref )
       shift
-      centos_branch=$1
+      out_ref=$1
       ;;
     --fedora-ref )
       shift
@@ -32,25 +54,68 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-git clone --depth 10 -b $centos_branch https://gitlab.com/redhat/centos-stream/rpms/llvm.git $centos_dir
-git clone --depth 10 -b $fedora_ref https://src.fedoraproject.org/rpms/llvm.git $fedora_dir
-if [ $bundle -eq 1 ]; then
-  sed -i 's/^%bcond_with bundle_compat_lib$/%bcond_without bundle_compat_lib/g' $fedora_dir/llvm.spec
+if [[ "$target" == "centos" && initialize -eq 1 ]]; then
+  error "cannot initialize CentOS repository."
 fi
 
-case "$centos_branch" in
-  *rhel-8*)
-    # RHEL-8 does not support rpmautospec completely.
-    sed -i \
-      -e 's/%autorelease/1%{?dist}/g' \
-      -e 's/%{?autochangelog}//g' \
-      -e 's/%{!?autochangelog:\(.*\)}/\1/g' \
-      $fedora_dir/llvm.spec
+case "$target" in
+  "centos" )
+    out_ref=${out_ref:-$CENTOS_REF}
+    out_dir=$CENTOS_DIR
+    ;;
+  "compat")
+    out_ref=${out_ref:-$COMPAT_REF}
+    out_dir=$COMPAT_DIR
+    ;;
+  * )
+    echo "unknown target $target"
+    exit 1
     ;;
 esac
 
-rsync --exclude-from=$script_dir/.centos-ignore --delete --cvs-exclude -av $fedora_dir/ $centos_dir/
+function centos_init() {
+  git clone --depth 10 -b $out_ref https://gitlab.com/redhat/centos-stream/rpms/llvm.git $out_dir
+}
 
-for f in $centos_dir/tests/*; do
-  sed -i 's~https://src.fedoraproject.org/tests/llvm.git~https://gitlab.com/redhat/centos-stream/tests/llvm.git~g' $f
-done
+function centos_post_sync() {
+  if [ $bundle -eq 1 ]; then
+    sed -i 's/^%bcond_with bundle_compat_lib$/%bcond_without bundle_compat_lib/g' $out_dir/llvm.spec
+  fi
+
+  case "$out_ref" in
+    *rhel-8*)
+      # RHEL-8 does not support rpmautospec completely.
+      sed -i \
+        -e 's/%autorelease/1%{?dist}/g' \
+        -e 's/%{?autochangelog}//g' \
+        -e 's/%{!?autochangelog:\(.*\)}/\1/g' \
+        $out_dir/llvm.spec
+      ;;
+  esac
+}
+
+function compat_init() {
+  if [ $initialize -eq 1 ]; then
+    test -d $out_dir || mkdir $out_dir
+  else
+    git clone --depth 10 -b $out_ref https://src.fedoraproject.org/rpms/llvm${maj_ver}.git $out_dir
+  fi
+}
+
+function compat_post_sync() {
+  sed -i 's/^%bcond_with compat_build$/%bcond_without compat_build/g' $out_dir/llvm.spec
+  mv $out_dir/llvm.spec  $out_dir/llvm${maj_ver}.spec
+}
+
+
+
+git clone --depth 10 -b $fedora_ref https://src.fedoraproject.org/rpms/llvm.git $fedora_dir
+maj_ver=$(awk '/^%global maj_ver / { print $3 }' ${fedora_dir}/llvm.spec)
+
+# Initialize the output directory.
+${target}_init
+
+rsync --exclude-from=$script_dir/.sync-ignore --delete --cvs-exclude -av $fedora_dir/ $out_dir/
+
+# Apply changes after synchronizing the files.
+${target}_post_sync
